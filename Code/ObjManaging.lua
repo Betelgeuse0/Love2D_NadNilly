9cf570ba500b005e03394096c74bba7163936175
Betelgeuse0 <kentorenshaw@gmail.com>
2020-12-30 22:04:23 -0700

    Physics

---
+++ Code/ObjManaging.lua
@@ -0,0 +1,18 @@
+
+objs = {}
+
+function objs.update(dt)
+	for i,o in ipairs(objs) do 
+		o:update(dt, i)
+	end
+end
+
+function objs.draw()
+	for i,o in ipairs(objs) do 
+		o:draw()
+	end
+end
+
+function objs.remove(i)
+	table.remove(objs, i)
+end
