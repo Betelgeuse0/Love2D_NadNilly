5c23dc863a47ad2043df5adce16d77704e823c59
Betelgeuse0 <kentorenshaw@gmail.com>
2020-12-31 22:51:39 -0700

    EGG

--- Code/Egg.lua
+++ Code/Egg.lua
@@ -1,13 +1,20 @@
-function Egg(x, y, file)
+function Egg(x, y, file, frames)
 	--local o = {update = EggUpdate, draw = EggDraw, image = love.graphics.newImage(imageFileName)}
-	local o = {update = EggUpdate, draw = EggDraw, anim = NewHorizontalAnimation(file, 43, 53, {0, 1, 2})}
-	local x1, y1 = x + 21, y + 4
-	local x2, y2 = x + 15, y - 17
-	local x3, y3 = x, y - 27
-	local x4, y4 = x - 15, y - 17
-	local x5, y5 = x - 21, y + 4
+	local o
+
+	if frames == nil then 
+		o = {update = EggUpdate, draw = EggDraw, image = love.graphics.newImage(file)}
+	else
+		o = {update = EggUpdate, draw = EggDraw, anim = NewHorizontalAnimation(file, 43, 53, frames)}
+	end
+
+	local x1, y1 = x + 22, y + 4
+	local x2, y2 = x + 15, y - 15
+	local x3, y3 = x, y - 26
+	local x4, y4 = x - 15, y - 15
+	local x5, y5 = x - 22, y + 4
 	local x6, y6 = x - 16, y + 17
-	local x7, y7 = x, y + 27
+	local x7, y7 = x, y + 25
 	local x8, y8 = x + 16, y + 17
 	SetUpPhysicsPolygon(o, "dynamic", 1, false, 5, x, y, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8)
 	table.insert(objs, o)
@@ -15,9 +22,16 @@ function Egg(x, y, file)
 end
 
 function EggUpdate(o, dt, i)
-	UpdateAnimation(dt, o.anim, 0.2)
+	if o.anim ~= nil then
+		UpdateAnimation(dt, o.anim, 0.2)
+	end
 end
 
 function EggDraw(o)
-	DrawPhysicsAnimation(o, o.anim, 22, 25)
+	--DrawPhysics(o, {0, 1, 0, 1})
+	if o.anim == nil then 
+		DrawPhysicsImage(o, o.image, 21, 27)
+	else
+		DrawPhysicsAnimation(o, o.anim, 21, 27)
+	end
 end
--- main.lua
+++ main.lua
@@ -2,20 +2,20 @@ require("Code/physicsExtra")
 require("Code/mathExtra")
 require("Code/animations")
 require("Code/Egg")
-require("Code/platform")
 require("Code/Björnio")
 require("Code/ObjManaging")
 require("Code/platform")
 
-
 function love.load()
+	love.graphics.setBackgroundColor(0, .4, .8, 1)
 	--love.window.setMode(800, 600)
 	--global variables lol
 	world = love.physics.newWorld(0, 9.81*64, true)
 	Björnio(400, 300, 80, 80, "dynamic", 0.5)
 	Platform(500, 300, 200, 5)
 
-	Egg(100, 200, "Sprites/goldegg-SpriteSheet.png")
+	Egg(100, 200, "Sprites/Eggs/goldegg-SpriteSheet.png", {0, 1, 2})
+	Egg(300, 200, "Sprites/Eggs/dogegg.png")
 
 	local ground = {}
 	ground.body = love.physics.newBody(world, 300, 500) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
@@ -30,6 +30,7 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.scale(0.5)
 	objs.draw()
 
 	love.graphics.setColor(0.28, 0.63, 0.05) -- set the drawing color to green for the ground
