d7d16600d4ea155dbe28585c07df6384564bb27a
AustinBoom <54464435+AustinBoom@users.noreply.github.com>
2020-12-31 22:49:22 -0700

    Adding Bjornio animations and updated sprites

--- main.lua
+++ main.lua
@@ -7,21 +7,34 @@ require("Code/Björnio")
 require("Code/ObjManaging")
 require("Code/platform")
 
+--Make window size bigger first thing
+love.window.setMode(980, 720)
+love.graphics.setBackgroundColor(0, .4, .8, 1)	--temporary backgruond color for vibes
 
 function love.load()
-	--love.window.setMode(800, 600)
 	--global variables lol
 	world = love.physics.newWorld(0, 9.81*64, true)
-	Björnio(400, 300, 80, 80, "dynamic", 0.5)
-	Platform(500, 300, 200, 5)
 
-	Egg(100, 200, "Sprites/goldegg-SpriteSheet.png")
+	--Platforms in vertical order
+	Platform(450, 300, 450, 5)
+	Platform(700, 150, 200, 5)
+	Platform(950, 0, 400, 5)
+	Platform(775, -180, 60, 5)
+	Platform(650, -350, 60, 5)
+	Platform(525, -500, 60, 5)
+	Platform(425, -650, 60, 5)
+	Platform(300, -800, 60, 5)
+
+	Egg(275, 100, "Sprites/goldegg-SpriteSheet.png")
 
 	local ground = {}
-	ground.body = love.physics.newBody(world, 300, 500) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
-  	ground.shape = love.physics.newRectangleShape(800, 50) --make a rectangle with a width of 650 and a height of 50
+	ground.body = love.physics.newBody(world, 512, 600) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
+  	ground.shape = love.physics.newRectangleShape(500, 30) --make a rectangle with a width of 650 and a height of 50
   	ground.fixture = love.physics.newFixture(ground.body, ground.shape, 0.5) --attach shape to body
   	objs.ground = ground
+
+  	--Spawn Bjornio last so he overlaps!
+  	Björnio(300, 200)
 end
 
 function love.update(dt)
